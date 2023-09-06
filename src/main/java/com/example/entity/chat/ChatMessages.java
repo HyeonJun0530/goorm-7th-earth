068b86450c68999717a0195957b81f631a30f064
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-07 01:50:39 +0900

    feat: 채팅 관련 엔티티 구현

---
+++ src/main/java/com/example/entity/chat/ChatMessages.java
@@ -0,0 +1,29 @@
+package com.example.entity.chat;
+
+import com.example.entity.BaseTimeEntity;
+import com.example.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "CHAT_MESSAGES")
+@Getter
+@NoArgsConstructor
+public class ChatMessages extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_messages_id")
+    private Long id;
+
+    private String messages;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+}
---
+++ src/main/java/com/example/entity/chat/ChatRoom.java
@@ -0,0 +1,27 @@
+package com.example.entity.chat;
+
+import com.example.entity.BaseTimeEntity;
+import com.example.entity.Member;
+import com.example.entity.goods.Goods;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "CHATROOM")
+@NoArgsConstructor
+@Getter
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_room_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goods_id")
+    private Goods goods;
+}
