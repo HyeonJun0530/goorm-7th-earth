70b0d92abc5379a139fc9bc614c1980365967c37
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-08 05:58:20 +0900

    fix:cors 진짜 해결

--- src/main/java/com/example/config/CorsConfig.java
+++ src/main/java/com/example/config/CorsConfig.java
@@ -1,16 +1,30 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST")
-                .maxAge(3000);
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true); //내서버가 응답을 할때 json을 자바스크립트에서 처리할 수 있게 할지
+        config.addAllowedOriginPattern("http://localhost:5173"); //모든 아이피를 응답허용
+        config.addAllowedHeader("*"); //모든 header 응답허용
+        //config.addExposedHeader("*");
+        config.addAllowedMethod("*"); //모든 post,get,put 허용
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
+
+
 }
