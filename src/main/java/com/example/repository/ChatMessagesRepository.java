1fa7c34fe1305f7c8292532a21d9f3486f0b24cb
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-07 20:31:48 +0900

    fix: 채팅 기능 삭제

--- src/main/java/com/example/repository/ChatMessagesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.chat.ChatMessages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessagesRepository extends JpaRepository<ChatMessages, Long> {
-}
--- src/main/java/com/example/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.chat.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
